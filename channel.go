f161f002043b4f933972b40648c034a3b5aecd5b
Oguz Bilgic <fisyonet@gmail.com>
2014-03-02 07:27:33 -0500

    Change Channel.dataChans's type to map[string][]chan interface{}

--- channel.go
+++ channel.go
@@ -1,22 +1,22 @@
 package pusher
 
 type Channel struct {
-	Name  string
-	binds map[string][]chan *Message
+	Name      string
+	dataChans map[string][]chan interface{}
 }
 
 func NewChannel(name string) *Channel {
-	return &Channel{name, make(map[string][]chan *Message)}
+	return &Channel{name, make(map[string][]chan interface{})}
 }
 
-func (c *Channel) Bind(event string) chan *Message {
-	eventChan := make(chan *Message)
-	c.binds[event] = append(c.binds[event], eventChan)
-	return eventChan
+func (c *Channel) Bind(event string) chan interface{} {
+	dataChan := make(chan interface{})
+	c.dataChans[event] = append(c.dataChans[event], dataChan)
+	return dataChan
 }
 
 func (c *Channel) processMessage(msg *Message) {
-	for _, eventChan := range c.binds[msg.Event] {
-		eventChan <- msg
+	for _, dataChan := range c.dataChans[msg.Event] {
+		dataChan <- msg.Data
 	}
 }
